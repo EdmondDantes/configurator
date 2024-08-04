72199e3978245128d2605a1fecea7aca083f0184
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-04 16:40:29 +0300

    + ConfigMain

---
+++ src/ConfigMain.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\Configurator;
+
+class ConfigMain                    extends ConfigIni
+{
+    public function __construct(string $appDir)
+    {
+        parent::__construct($appDir . '/config/main.ini');
+    }
+}
