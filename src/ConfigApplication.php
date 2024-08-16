f063d57f7497f9cdfba5ff3d684aa3d55457dcfc
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-16 09:22:49 +0300

    + ConfigApplication

--- src/ConfigApplication.php
+++ src/ConfigApplication.php
@@ -3,11 +3,15 @@ declare(strict_types=1);
 
 namespace IfCastle\Configurator;
 
+use IfCastle\Application\Bootloader\BootloaderExecutorInterface;
+use IfCastle\Application\Bootloader\BootloaderInterface;
 use IfCastle\Application\Bootloader\Builder\ZeroContextInterface;
 use IfCastle\Application\Bootloader\Builder\ZeroContextRequiredInterface;
+use IfCastle\ServiceManager\RepositoryStorages\RepositoryReaderByScopeInterface;
+use IfCastle\ServiceManager\RepositoryStorages\RepositoryReaderInterface;
 
 final class ConfigApplication       extends ConfigIni
-                                    implements ZeroContextRequiredInterface
+                                    implements ZeroContextRequiredInterface, BootloaderInterface
 {
     public function __construct() { parent::__construct('!undefined!'); }
     
@@ -17,5 +21,15 @@ final class ConfigApplication       extends ConfigIni
     {
         $this->file                 = $zeroContext->getApplicationDirectory() . '/main.ini';
         return $this;
+    }
+    
+    #[\Override]
+    public function buildBootloader(BootloaderExecutorInterface $bootloaderExecutor): void
+    {
+        $bootloaderExecutor->getBootloaderContext()->getSystemEnvironmentBootBuilder()
+            ->bindObject(
+                [RepositoryReaderInterface::class, RepositoryReaderByScopeInterface::class],
+                new ServiceConfig($this->file)
+            );
     }
 }
--- src/ServiceConfig.php
+++ src/ServiceConfig.php
@@ -3,19 +3,41 @@ declare(strict_types=1);
 
 namespace IfCastle\Configurator;
 
+use IfCastle\ServiceManager\RepositoryStorages\RepositoryReaderByScopeInterface;
 use IfCastle\ServiceManager\RepositoryStorages\RepositoryReaderInterface;
 
 class ServiceConfig                 extends ConfigIni
-                                    implements RepositoryReaderInterface
+                                    implements RepositoryReaderInterface, RepositoryReaderByScopeInterface
 {
     public function __construct(string $appDir)
     {
         parent::__construct($appDir . '/services.ini');
     }
     
+    #[\Override]
     public function getServicesConfig(): array
     {
         $this->load();
         return $this->data;
+    }
+    
+    #[\Override]
+    public function getServicesConfigByScope(string ...$scopes): array
+    {
+        $this->load();
+        
+        $servicesConfig             = [];
+        
+        foreach($this->data as $service => $config) {
+            
+            // If any $scopesIncluded item is in $scopes, then add the service to $servicesConfig
+            // or if any $scopesExcluded item is in $scopes, then skip the service
+            if(count(array_intersect($config['scopes'] ?? [], $scopes)) > 0
+            || count(array_intersect($config['scopesExcluded'] ?? [], $scopes)) === 0) {
+                $servicesConfig[$service] = $config;
+            }
+        }
+        
+        return $servicesConfig;
     }
 }
