4a2ac0fc4f0aae0e23a8e9c51bb403d350b8e93d
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-11-02 19:27:20 +0200

    + PHPTest

--- tests/ServiceConfigTest.php
+++ tests/ServiceConfigTest.php
@@ -25,7 +25,7 @@ class ServiceConfigTest extends ServiceConfigTestCase
             _service_name_ = 'service2'
             ; service configuration
             class = 'ServiceClass2'
-            isActive = false
+            isActive = true
             tags[] = 'tag5'
             tags[] = 'tag6'
             excludeTags[] = 'tag7'
@@ -40,19 +40,19 @@ class ServiceConfigTest extends ServiceConfigTestCase
         $result                     = $serviceConfig->findServiceConfig('service1');
 
         $this->assertEquals('ServiceClass1', $result['class']);
-        $this->assertTrue($result['isActive']);
+        $this->assertTrue($result['isActive'], 'Service must be active');
         $this->assertEquals(['tag1', 'tag2'], $result['tags']);
         $this->assertEquals(['tag3', 'tag4'], $result['excludeTags']);
 
         $result                     = $serviceConfig->findServiceConfig('service2');
 
         $this->assertEquals('ServiceClass2', $result['class']);
-        $this->assertFalse($result['isActive']);
+        $this->assertTrue($result['isActive'], 'Service must be active');
         $this->assertEquals(['tag5', 'tag6'], $result['tags']);
         $this->assertEquals(['tag7', 'tag8'], $result['excludeTags']);
     }
 
-    public function testFindServicesConfigByPackage(): void
+    public function testServiceCollection(): void
     {
         $file                       = $this->appDir . '/services.ini';
 
@@ -96,29 +96,30 @@ class ServiceConfigTest extends ServiceConfigTestCase
 
         $serviceConfig              = new ServiceConfig($this->appDir);
 
-        $services                   = $serviceConfig->findServicesConfigByPackage('package1');
+        $services                   = $serviceConfig->getServiceCollection(packageName: 'package1');
 
         $this->assertIsArray($services, 'Services must be an array');
-        $this->assertCount(2, $services, 'Services must have 2 elements');
+        $this->assertArrayHasKey('service1', $services, 'Services must have service1 key');
+        $this->assertCount(2, $services['service1'], 'Services1 must have 2 elements');
 
-        $this->assertEquals('ServiceClass1', $services[0]['class']);
-        $this->assertTrue($services[0]['isActive']);
-        $this->assertEquals(['tag1', 'tag2'], $services[0]['tags']);
-        $this->assertEquals(['tag3', 'tag4'], $services[0]['excludeTags']);
+        $this->assertEquals('ServiceClass1', $services['service1']['0']['class']);
+        $this->assertTrue($services['service1']['0']['isActive']);
+        $this->assertEquals(['tag1', 'tag2'], $services['service1']['0']['tags']);
+        $this->assertEquals(['tag3', 'tag4'], $services['service1']['0']['excludeTags']);
 
-        $this->assertEquals('ServiceClass2', $services[1]['class']);
-        $this->assertFalse($services[1]['isActive']);
-        $this->assertEquals(['tag5', 'tag6'], $services[1]['tags']);
-        $this->assertEquals(['tag7', 'tag8'], $services[1]['excludeTags']);
+        $this->assertEquals('ServiceClass2', $services['service1']['1']['class']);
+        $this->assertFalse($services['service1']['1']['isActive']);
+        $this->assertEquals(['tag5', 'tag6'], $services['service1']['1']['tags']);
+        $this->assertEquals(['tag7', 'tag8'], $services['service1']['1']['excludeTags']);
 
-        $services                   = $serviceConfig->findServicesConfigByPackage('package2');
+        $services                   = $serviceConfig->getServiceCollection(packageName: 'package2');
 
         $this->assertIsArray($services, 'Services must be an array');
         $this->assertCount(1, $services, 'Services must have 1 element');
 
-        $this->assertEquals('ServiceClass3', $services[0]['class']);
-        $this->assertTrue($services[0]['isActive']);
-        $this->assertEquals(['tag5', 'tag6'], $services[0]['tags']);
+        $this->assertEquals('ServiceClass3', $services['service1']['2']['class']);
+        $this->assertTrue($services['service1']['2']['isActive']);
+        $this->assertEquals(['tag5', 'tag6'], $services['service1']['2']['tags']);
     }
 
 }
